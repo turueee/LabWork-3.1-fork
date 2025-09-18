5e9fce89473d10534678a75f6a692e52b20075a9
turueee <turushkine@inbox.ru>
2025-09-18 17:51:42 +0300

    Set

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/turus/Desktop/LabWork-3.1-fork
+# Install script for directory: D:/Рабочий стол/LabWork-3.1-fork
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,17 +34,17 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/turus/Desktop/LabWork-3.1-fork/build/src/cmake_install.cmake")
-  include("C:/Users/turus/Desktop/LabWork-3.1-fork/build/samples/cmake_install.cmake")
-  include("C:/Users/turus/Desktop/LabWork-3.1-fork/build/gtest/cmake_install.cmake")
-  include("C:/Users/turus/Desktop/LabWork-3.1-fork/build/test/cmake_install.cmake")
+  include("D:/Рабочий стол/LabWork-3.1-fork/build/src/cmake_install.cmake")
+  include("D:/Рабочий стол/LabWork-3.1-fork/build/samples/cmake_install.cmake")
+  include("D:/Рабочий стол/LabWork-3.1-fork/build/gtest/cmake_install.cmake")
+  include("D:/Рабочий стол/LabWork-3.1-fork/build/test/cmake_install.cmake")
 
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/turus/Desktop/LabWork-3.1-fork/build/install_local_manifest.txt"
+  file(WRITE "D:/Рабочий стол/LabWork-3.1-fork/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -60,6 +60,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/turus/Desktop/LabWork-3.1-fork/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/Рабочий стол/LabWork-3.1-fork/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
